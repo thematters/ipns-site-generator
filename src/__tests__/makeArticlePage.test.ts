563dba8474da24000842e3f8c458d0f1fd024634
gitwoz <177856586+gitwoz@users.noreply.github.com>
2025-04-10 13:51:38 +0700

    feat(makeArticlePage): add skipAssets option to exclude asset processing; remove paymentPointer support

--- src/__tests__/makeArticlePage.test.ts
+++ src/__tests__/makeArticlePage.test.ts
@@ -2,6 +2,7 @@ import fetch from 'isomorphic-fetch'
 import { makeArticlePage } from '..'
 import { MOCK_ARTICLE_PAGE } from '../render/mock'
 import { deserializeHtmlBundle } from './utils'
+import * as cheerio from 'cheerio'
 
 jest.mock('isomorphic-fetch')
 const mockedFetch = fetch as jest.Mock
@@ -16,6 +17,10 @@ describe('makeArticlePage', () => {
 
     const { bundle } = await makeArticlePage(MOCK_ARTICLE_PAGE_DATA)
     expect(deserializeHtmlBundle(bundle)).toMatchSnapshot()
+
+    // Image src should be relative path
+    const $ = cheerio.load(bundle[0]?.content.toString('utf-8')!)
+    expect($('img').attr('src')).not.toContain('https://')
   })
 
   test('can generate basic HTML bundle when `getAsset` fails', async () => {
@@ -27,15 +32,22 @@ describe('makeArticlePage', () => {
     expect(deserializeHtmlBundle(bundle)).toMatchSnapshot()
   })
 
-  test('can generate HTML bundle with payment pointer', async () => {
+  test('skipAssets option should not include assets in bundle', async () => {
     mockedFetch.mockResolvedValue({
       arrayBuffer: () => Promise.resolve(new ArrayBuffer(1)),
     })
 
-    const { bundle } = await makeArticlePage({
+    const { bundle: bundleWithoutAssets } = await makeArticlePage({
       ...MOCK_ARTICLE_PAGE_DATA,
-      paymentPointer: '$pay-me',
+      skipAssets: true,
     })
-    expect(deserializeHtmlBundle(bundle)).toMatchSnapshot()
+
+    expect(bundleWithoutAssets.length).toBe(1)
+    expect(bundleWithoutAssets[0]?.path).toBe('index.html')
+    expect(deserializeHtmlBundle(bundleWithoutAssets)).toMatchSnapshot()
+
+    // Image src should be absolute path
+    const $ = cheerio.load(bundleWithoutAssets[0]?.content.toString('utf-8')!)
+    expect($('img').attr('src')).toContain('https://')
   })
 })
